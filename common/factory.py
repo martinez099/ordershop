a30fe592bb007c5c8858426d2aae3929f8e43814
Martin Forstner <martin@redislabs.com>
2019-02-22 17:43:57 +0100

    document factory

--- common/factory.py
+++ common/factory.py
@@ -3,6 +3,13 @@ import uuid
 
 
 def create_customer(_name, _email):
+    """
+    Create a customer entity.
+
+    :param _name: The name of the customer.
+    :param _email: The email address of the customer.
+    :return: A dict with the entity properties.
+    """
     return {
         'id': str(uuid.uuid4()),
         'name': _name,
@@ -11,6 +18,12 @@ def create_customer(_name, _email):
 
 
 def create_billing(_order_id):
+    """
+    Create a billing entity.
+
+    :param _order_id: The order ID the billing belongs to.
+    :return: A dict with the entity properties.
+    """
     return {
         'id': str(uuid.uuid4()),
         'order_id': _order_id,
@@ -19,6 +32,13 @@ def create_billing(_order_id):
 
 
 def create_inventory(_product_id, _amount):
+    """
+    Create an inventory entity.
+
+    :param _product_id: The product ID the inventory is for.
+    :param _amount: The amount of products in the inventory.
+    :return: A dict with the entity properties.
+    """
     return {
         'id': str(uuid.uuid4()),
         'product_id': _product_id,
@@ -27,6 +47,13 @@ def create_inventory(_product_id, _amount):
 
 
 def create_order(_product_ids, _customer_id):
+    """
+    Create an order entity.
+
+    :param _product_ids: The product IDs the order is for.
+    :param _customer_id: The customer ID the order is made by.
+    :return: A dict with the entity properties.
+    """
     return {
         'id': str(uuid.uuid4()),
         'product_ids': _product_ids,
@@ -35,6 +62,13 @@ def create_order(_product_ids, _customer_id):
 
 
 def create_product(_name, _price):
+    """
+    Create a product entity.
+
+    :param _name: The name of the product.
+    :param _price: The price of the product.
+    :return: A dict with the entity properties.
+    """
     return {
         'id': str(uuid.uuid4()),
         'name': _name,
